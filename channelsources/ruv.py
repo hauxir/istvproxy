d27aca896ce55c3af01631da5eeeaadefdd165a3
Haukur Rosinkranz <hauxir@gmail.com>
2021-04-16 14:01:01 +0000

    fix ruv url

--- channelsources/ruv.py
+++ channelsources/ruv.py
@@ -2,8 +2,8 @@ import requests
 
 from channelsource import ChannelSource, USER_AGENT
 
+
 RUV_ROOT_URL = 'http://ruvruv-live.hls.adaptive.level3.net/ruv'
-STREAM_URL = RUV_ROOT_URL + '/%s/index.m3u8'
 
 
 class RUVChannels(ChannelSource):
@@ -12,8 +12,10 @@ class RUVChannels(ChannelSource):
         return ['ruv', 'ruv2']
 
     def get_channel_playlist(self, name):
+        json_response = requests.get(f"https://geo.spilari.ruv.is/channel/{name}").json()
+        url = json_response["url"]
         playlist = requests.get(
-            STREAM_URL % name, headers={'User-Agent': USER_AGENT}).content
+            url, headers={'User-Agent': USER_AGENT}).content
         playlist = playlist.replace(
            'index', RUV_ROOT_URL + '/%s/index' % name
         )
